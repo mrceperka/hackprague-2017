24222ed6329023072e17b8d5dc623ac155e5765a
Mr Ceperka <johnnychudomsky@gmail.com>
2017-06-17 23:28:03 +0200

    find by code

--- src/Root.js
+++ src/Root.js
@@ -27,9 +27,9 @@ export default () => {
             <Switch>
               <Route exact path="/" component={Home} />
               <Route exact path="/boards" component={Boards} />
+              <Route exact path="/boards/:id" component={BoardDetail} />
               <Route exact path="/boards/new" component={NewBoard} />
               <Route exact path="/boards/edit/:id" component={EditBoard} />
-              <Route exact path="/boards/:id" component={BoardDetail} />
             </Switch>
           </div>
         </div>
