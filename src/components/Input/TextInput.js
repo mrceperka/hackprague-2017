93aa5e5b8cffb93b9af56abeebd989ee127dcf18
Marek Lisy <marek.lisy.hk@gmail.com>
2017-06-27 20:11:36 +0200

    refactor BoardNew page

---
+++ src/components/Input/TextInput.js
@@ -0,0 +1,63 @@
+import React from "react";
+import {
+  FormGroup,
+  Label,
+  Input,
+  InputGroup,
+  InputGroupAddon,
+  FormText
+} from "reactstrap";
+import uuid from "uuid";
+
+class TextInput extends React.Component {
+  componentWillMount() {
+    this.id = uuid.v4();
+  }
+
+  render() {
+    const {
+      label,
+      icon,
+      autoFocus,
+      type,
+      name,
+      value,
+      onChange,
+      subtitle,
+      children,
+      ...rest
+    } = this.props;
+    return (
+      <FormGroup>
+        <Label for={this.id}>
+          {label}
+          {subtitle
+            ? <FormText color="muted">
+                {subtitle}
+              </FormText>
+            : null}
+        </Label>
+        <InputGroup>
+          {icon
+            ? <InputGroupAddon>
+                <i className="material-icons">{icon}</i>
+              </InputGroupAddon>
+            : null}
+          <Input
+            autoFocus={autoFocus}
+            type={type}
+            id={this.id}
+            name={name}
+            value={value}
+            onChange={onChange}
+            {...rest}
+          >
+            {children}
+          </Input>
+        </InputGroup>
+      </FormGroup>
+    );
+  }
+}
+
+export default TextInput;
---
+++ src/components/Input/index.js
@@ -0,0 +1,3 @@
+export { default as CheckpointInput } from "./CheckpointInput";
+export { default as RadioInput } from "./RadioInput";
+export { default as TextInput } from "./TextInput";
